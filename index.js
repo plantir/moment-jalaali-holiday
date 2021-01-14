a23ef8f86bcc0a6b60d0bbcc359d54cd1205cd32
armin <arminkheirkhahan@gmail.com>
2021-01-14 11:30:40 +0330

    set current locale to en

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const moment = require("moment-jalaali");
 const momentHijri = require("moment-hijri");
+momentHijri.locale("en");
 const events = require("./events.json");
 moment.fn.isHoliday = function () {
   let isHoliday = false;
-  let hijriDate = momentHijri(this)
+  let hijriDate = momentHijri(this);
   events.PersianCalendar.forEach((day) => {
-    if (day.day === this.jDate() && day.month === this.jMonth()+1) {
+    if (day.day === this.jDate() && day.month === this.jMonth() + 1) {
       isHoliday = day.holiday ? true : isHoliday;
     }
   });
   events.HijriCalendar.forEach((day) => {
-    if (day.day === hijriDate.iDate() && day.month === hijriDate.iMonth()+1) {
+    if (day.day === hijriDate.iDate() && day.month === hijriDate.iMonth() + 1) {
       isHoliday = day.holiday ? true : isHoliday;
     }
   });
