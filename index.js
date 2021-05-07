02a217907f44efb55836d2df2629204168b4dbed
armin <arminkheirkhahan@gmail.com>
2021-05-07 11:33:44 +0430

    fix 1400 holiday

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const events = require("./events.json");
 moment.fn.isHoliday = function () {
   let isHoliday = false;
   let hijriDate = momentHijri(this);
-  events.PersianCalendar.forEach((day) => {
+  events.PersianCalendar.filter(item=> item.type == 'Iran').forEach((day) => {
     if (day.day === this.jDate() && day.month === this.jMonth() + 1) {
       isHoliday = day.holiday ? true : isHoliday;
     }
   });
-  events.HijriCalendar.forEach((day) => {
+  events.HijriCalendar.filter(item => item.type == 'Islamic Iran').forEach((day) => {
     if (day.day === hijriDate.iDate() && day.month === hijriDate.iMonth() + 1) {
       isHoliday = day.holiday ? true : isHoliday;
     }
