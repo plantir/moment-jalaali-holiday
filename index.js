041daad5d9228b0c50a839f913856f69350a5327
armin <armin@smileitsolutions.uk>
2023-03-13 20:29:07 +0330

    fix 1402

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const events = require("./events.json");
 moment.fn.isHoliday = function () {
   let isHoliday = false;
   let hijriDate = momentHijri(this);
-  events.PersianCalendar.filter(item=> item.type == 'Iran').forEach((day) => {
-    if (day.day === this.jDate() && day.month === this.jMonth() + 1) {
-      isHoliday = day.holiday ? true : isHoliday;
+  events.PersianCalendar.filter((item) => item.type == "Iran").forEach(
+    (day) => {
+      if (day.day === this.jDate() && day.month === this.jMonth() + 1) {
+        isHoliday = day.holiday ? true : isHoliday;
+      }
     }
-  });
-  events.HijriCalendar.filter(item => item.type == 'Islamic Iran').forEach((day) => {
-    if (day.day === hijriDate.iDate() && day.month === hijriDate.iMonth() + 1) {
-      isHoliday = day.holiday ? true : isHoliday;
+  );
+  events.HijriCalendar.filter((item) => item.type == "Islamic Iran").forEach(
+    (day) => {
+      if (
+        day.day === hijriDate.iDate() &&
+        day.month === hijriDate.iMonth() + 1
+      ) {
+        isHoliday = day.holiday ? true : isHoliday;
+      }
     }
-  });
+  );
   return isHoliday;
 };
 
